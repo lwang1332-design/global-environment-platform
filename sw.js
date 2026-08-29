c13ea820696710fbbff52d7ebbe69e797e6dec9d
lwang1332-design <lwang1332@gmail.com>
2026-08-30 01:55:36 +0800

    Add service worker

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='global-env-v2.6-shell-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(e.request.method!=='GET') return;
+  if(u.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))));
+  }
+});
