85aea9a9b4a698c20ab21fed760a476347226ce7
lwang1332-design <lwang1332@gmail.com>
2026-08-30 16:17:41 +0800

    Cache engineering UI assets for PWA

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='global-env-v2.8-offline-world-v1';
+const CACHE='global-env-v2.8-engineering-ui-v1';
 const TILE_CACHE='global-env-map-tiles-v1';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icons/app-icon.svg','./map-tier.js','./tiles/manifest.json','./assets/goldwind-logo.png'];
+const SHELL=['./','./index.html','./manifest.webmanifest','./icons/app-icon.svg','./map-tier.js','./tiles/manifest.json','./assets/goldwind-logo.png','./assets/ui-engineering.css','./assets/ui-engineering.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE&&k!==TILE_CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
