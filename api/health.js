7bc0fd43c6fca6f111a923081b689a8c279d0360
lwang1332-design <lwang1332@gmail.com>
2026-08-31 15:09:10 +0800

    Use non-route V2.9 server libraries

--- api/health.js
+++ api/health.js
@@ -1,3 +1,3 @@
 'use strict';
-const {setCors,json}=require('./_lib/security');
+const {setCors,json}=require('../server/lib/security');
 module.exports=async function(req,res){setCors(req,res);if(req.method==='OPTIONS')return json(res,204,{});return json(res,200,{ok:true,service:'global-environment-config',schemaVersion:'2.9',time:new Date().toISOString()})};
