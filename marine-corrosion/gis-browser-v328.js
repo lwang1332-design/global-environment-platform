e1774a326f8102d90bba1674a7d5b6475e0490c8
lwang1332-design <lwang1332@gmail.com>
2026-09-13 13:13:14 +0800

    feat(marine-corrosion): route GIS compatibility entry to V3.3.1

--- marine-corrosion/gis-browser-v328.js
+++ marine-corrosion/gis-browser-v328.js
@@ -1,2 +1,2 @@
-// V3.3.0 compatibility entry. The V3.2.8 implementation is archived under legacy-v328/.
-export * from './gis-browser-v330.js';
+// V3.3.1 compatibility entry. The V3.2.8 implementation is archived under legacy-v328/.
+export * from './gis-browser-v331.js';
