7459cf3415c5872a2f1b8d0b115fe5aa9c4e086c
lwang1332-design <lwang1332@gmail.com>
2026-09-15 09:24:21 +0800

    chore(marine-corrosion): define V3.4.0 final version boundaries

---
+++ marine-corrosion/input-policy-v340.js
@@ -0,0 +1,7 @@
+export * from './input-policy-v333.js';
+export const APP_VERSION='3.4.0';
+export const SCIENCE_MODEL_VERSION='3.3.0';
+export const DATA_LAYER_VERSION='3.3.2';
+export const DIAGNOSTIC_LAYER_VERSION='3.3.3';
+export const COASTAL_ASSESSMENT_VERSION='3.4.0';
+export const STANDARD_BASELINE={iso9223:'ISO 9223:2012',iso9225:'ISO 9225:2012',iso9225Draft:'ISO/DIS 9225 Edition 3 - not normative until published'};
